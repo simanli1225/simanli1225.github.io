dc8b22816a1f17b78d4553a2ea882c2ebe076759
Siman <sl5858@nyu.edu>
2021-10-19 14:44:32 -0400

    try with the video image

--- js/M1SpatialAudioPlayer.js
+++ js/M1SpatialAudioPlayer.js
@@ -267,20 +267,20 @@ async function setupCamera() {
 async function renderPrediction() {
   const predictions = await model.estimateFaces(video);
   const warningMessage = "WARNING: UNABLE TO TRACK FACE!";
-  // ctx.drawImage(
-  //   video,
-  //   0,
-  //   0,
-  //   videoWidth,
-  //   videoHeight,
-  //   0,
-  //   0,
-  //   canvas.width,
-  //   canvas.height
-  // );
+  ctx.drawImage(
+    video,
+    0,
+    0,
+    videoWidth,
+    videoHeight,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
 
   //get rid of the video
-  ctx.drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, 0, 0);
+  // ctx.drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, 0, 0);
   //draw black image
   ctx.fillStyle = "rgb(0,0,0)";
   ctx.fillRect(0,0, canvas.width, canvas.height);
