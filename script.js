fb689d7f3ddfd88f2276bfc478f37627232602c4
Siman <sl5858@nyu.edu>
2021-10-21 15:47:30 -0400

    icons back

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-var playBtn = document.getElementById("play_button_id"),
-
-var legal = `<div class="legal">
+let playBtn = document.getElementById("play_button_id");
+let legal = `<div class="legal">
 <div id="legal_top">
-  <img id="icon_logo" src="source/logo.svg" alt="" />
-</div>
+  <img id="icon_logo" src="source/logo.svg"/></div>
 <div id="legal_bottom">
   <div class="legal_bottom_cl" id="legal_bottom_left">
-    <p id="legal-item_id_1" class="legal-item clickable"><a href="https://www.squarespace.com/privacy" target="_blank" >PRIVACY POLICY</a></p>
-    <p id="legal-item_id_2" class="legal-item clickable"><a href="https://www.squarespace.com/terms-of-service" target="_blank" >TERMS OF SERVICE</a></p>
+    <p id="legal-item_id_1" class="legal-item clickable"><a href="https://www.squarespace.com/privacy" target="_blank">PRIVACY POLICY</a></p>
+    <p id="legal-item_id_2" class="legal-item clickable"><a href="https://www.squarespace.com/terms-of-service" target="_blank">TERMS OF SERVICE</a></p>
   </div>
   <div class="legal_bottom_cl" id="legal_bottom_right">
     <img
@@ -27,7 +25,7 @@ var legal = `<div class="legal">
 </div>`;
 appendHtml(document.body, legal);
 function appendHtml(el, str) {
-  var div = document.createElement("div"); //container to append to
+let div = document.createElement("div"); //container to append to
   div.innerHTML = str;
   while (div.children.length > 0) {
     el.appendChild(div.children[0]);
@@ -56,12 +54,5 @@ function playButton(){
     // i is even
     Player.play();
     console.log("now play music");
-
   }
-  // Player.play();
-  // <button onclick="Player.play()" class="button">Play</button>
-  {/* <button onclick="Player.stop()" class="button">Stop</button> */}
-  // playBtn.innerHTML = tl.paused() ? "play" : "pause";
-  // playBtn.innerHTML = "pause";
-
 }
